f073b90120f1b0d45231354286ec1f0b7feaf5fa
V-Coder <vahidgh.vgh@gmail.com>
2022-05-26 20:40:57 +0430

    Add totalSupply simulator

---
+++ simulate/simulate.py
@@ -0,0 +1,39 @@
+from multicall.call import Call
+from multicall.multicall import Multicall
+from web3 import Web3
+import matplotlib.pyplot as plt
+
+
+def get_points():
+    start = 33657905
+    diff_block = 86400
+    end = 33657905 + diff_block
+    days = 3
+    calls = []
+    total_supply = []
+    times = []
+    for _ in range(days):
+        for time in range(start, end, 360):
+            calls.append(
+                Call(
+                    w3,
+                    contract_address,
+                    ["totalSupplyAt(uint256)(uint256)", time],
+                    [[time, None]],
+                )
+            )
+        points = Multicall(w3, calls)()
+        for point in points:
+            total_supply.append(points[point][0] / 10**18)
+            times.append(point)
+        start += diff_block
+        end += diff_block
+    fig, ax = plt.subplots()
+    ax.plot(times, total_supply)
+
+
+if __name__ == "__main__":
+    contract_address = "0x8B42c6Cb07c8dD5fE5dB3aC03693867AFd11353d"
+    w3 = Web3(Web3.HTTPProvider("https://rpc.ftm.tools"))
+    get_points()
+    plt.show()
